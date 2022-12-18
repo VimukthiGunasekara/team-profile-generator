793cb6d8ba5bb5966841d60f5773c47008278cfc
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2022-12-18 08:48:19 -0500

    develop HTML template file and classes

---
+++ src/template.js
@@ -0,0 +1,96 @@
+const renderTeam = team => {
+    const renderManager = team => {
+        return `
+            <div class="card employee-card p-3 m-2">
+                <div class="card-header bg-dark   text-white">
+                    <h2 class="card-title">${manager.getName()}</h2>
+                    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${manager.getId()}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                        <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                    </ul>
+                </div>
+            </div>`;
+    }
+
+    const renderEngineer = engineer => {
+        return `<div class="card employee-card p-3 m-2">
+            <div class="card-header bg-secondary text-white">
+                <h2 class="card-title">${engineer.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank"
+                        rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                </ul>
+            </div>
+        </div>`;
+    }
+
+    const renderIntern = intern => {
+        return `<div class="card employee-card p-3 m-2">
+            <div class="card-header bg-secondary text-white">
+                <h2 class="card-title">${intern.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${intern.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                    <li class="list-group-item">School: ${intern.getSchool()}</li>
+                </ul>
+            </div>
+        </div>`;
+    }
+
+    
+
+}
+
+function renderHTML() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+      <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+      <title>Team Profile Generator</title>
+    </head>
+    <style>
+      .bg-color {
+        background-color: rgb(199, 167, 230);
+      }
+    </style>
+    
+    <body>
+    
+      <div class="jumbotron bg-color p-3">
+        <div class="container text-center">
+          <h1>Team Profile Generator</h1>
+          <p>information about employees on a software engineering team.</p>
+        </div>
+      </div>
+    
+      <div class="container">
+        <div class="row p-3">
+          <div class="row team-area col-12 d-flex justify-content-center">
+            ${renderTeam(team)}
+          </div>
+        </div>
+      </div>
+    
+    </body>
+    
+    </html>`;
+}
+
+module.exports = { renderHTML };
